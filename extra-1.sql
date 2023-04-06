f0efc4132aa3620eb9c17e00b5b19a038c561fc1
KatWei67 <129415169+KatWei67@users.noreply.github.com>
2023-04-06 08:33:35 -0700

    Update extra-1.sql

--- extra-1.sql
+++ extra-1.sql
@@ -1,3 +1,5 @@
 SELECT age FROM persons WHERE gender = 'Female' AND city = 'West Palm Beach' AND state = 'Florida' AND title = 'Dr';
 
 SELECT age FROM persons WHERE gender = 'Male' AND city = 'Phoenix' AND state = 'Arizona' AND title = 'Rev';
+
+/* No, they have different age.
